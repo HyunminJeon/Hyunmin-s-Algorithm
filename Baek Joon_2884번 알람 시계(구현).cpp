b8224fc91a79e177817a7a232ce441970f98916f
HyunminJeon <33923117+HyunminJeon@users.noreply.github.com>
2020-06-04 21:51:29 +0900

    Add files via upload

---
+++ Baek Joon_2884번 알람 시계(구현).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int H, M;
+    cin >> H >> M;
+
+    if (M >= 45 && M <= 59) {
+        cout << H << " " << M - 45 << endl;
+    }
+    else if (M >= 0 && M <= 44) {
+        if (H != 0) {
+        cout << H - 1 << " " << 60 - (45 - M) << endl;
+        }
+        else if (H == 0) {
+            cout << 23 << " " << 60 - (45 - M) << endl;
+        }
+    }
+
+    return 0;
+
+}
